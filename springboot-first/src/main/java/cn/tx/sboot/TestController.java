1460ace5840eef4019c02562ec5ea9eecf145e0a
kevin <klian@colgate.edu>
2021-06-29 23:41:24 +0800

    [feat] initial commit

---
+++ springboot-first/src/main/java/cn/tx/sboot/TestController.java
@@ -0,0 +1,14 @@
+package cn.tx.sboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello spring boot";
+    }
+
+}
